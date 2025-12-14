26267ef31c805e80ce2b4990c98ebeda4876ba7f
codedbytriloki <trilokisaini61@gmail.com>
2025-12-14 22:41:02 +0530

    Fix header background path and navbar classes; use imported asset

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
 
   return (
     <div className="absolute top-0 left-0 w-full z-10">
-      <div className='Container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
+      <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
         <img src={assets.logo} alt="" />
         <ul className='hidden md:flex gap-7 text-white'>
           <a href="#Header" className='cursor-pointer hover:text-gray-400'>Home</a>
@@ -27,10 +27,10 @@ const Navbar = () => {
           <a href="#Testimonails" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
         </ul>
         <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign Up</button>
-        <img onClick={() => setShowMenu(true)} src={assets.menu_icon} alt="" className='md:hidden w-7 cursor-pointe' />
+        <img onClick={() => setShowMenu(true)} src={assets.menu_icon} alt="" className='md:hidden w-7 cursor-pointer' />
       </div>
       {/* --------------- mobile - menu ------------- */}
-      <div className={`md:hidden ${showMenu ? 'fixed w-700' : 'h-0 w-0'}  right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
+      <div className={`md:hidden ${showMenu ? 'fixed w-[300px]' : 'h-0 w-0'} right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
         <div className='flex justify-end p-6 cursor-pointer'>
           <img onClick={() => setShowMenu(false)} src={assets.cross_icon} className="w-4" />
         </div>
