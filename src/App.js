e52cd6f12524cd10c838a3d54b928a78a47e5980
codedbytriloki <trilokisaini61@gmail.com>
2025-12-14 21:05:15 +0530

    Estate clone

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import Projects from './components/Projects';
+import Testimonails from './components/Testimonails';
+
+
+
+function App() {
+  return (
+    <div className='w-full overflow-hidden scroll-smooth'>
+      <Header />
+      <About />
+      <Projects />
+      <Testimonails />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
